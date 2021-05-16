aa078c7a098da2896b2b9ccaad7de06e9b97033d
Tom Brooks <tom.g.r.brooks@gmail.com>
2021-05-16 19:43:25 +0100

    Use functional components for skill tree

--- components/skillTree.js
+++ components/skillTree.js
@@ -1,59 +1,52 @@
 import styles from '../styles/skilltree.module.scss'
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import Sunburst from './Sunburst'
 import data from '../utils/skills'
 
-class SkillTree extends Component {
+function useWindowSize() {
+  const [size, setSize] = useState([0, 0])
+  useEffect(() => {
+    function updateSize() {
+      let totalWidth = Math.min(600, window.innerWidth)
+      setSize([totalWidth, totalWidth]);
+    }
+    window.addEventListener('resize', updateSize)
+    updateSize()
+    return () => window.removeEventListener('resize', updateSize)
+  }, [])
+  return size
+}
 
-  constructor(props) {
-    super(props);
-    this.state = { width: 0., height: 0. };
-    this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
-  }
+const SkillTree = () => {
+  const [width, height] = useWindowSize()
 
-  componentDidMount() {
-    this.updateWindowDimensions();
-    window.addEventListener('resize', this.updateWindowDimensions);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('resize', this.updateWindowDimensions);
-  }
-
-  updateWindowDimensions() {
-    let totalWidth = Math.min(600, window.innerWidth)
-    this.setState({ width: totalWidth, height: totalWidth });
-  }
-
-  render() {
-    const tooltipContent = <div className="sunburstTooltip" style="display: inline;
-                                                                   position: absolute;
-                                                                   max-width: 320px;
-                                                                   white-space: nowrap;
-                                                                   font: 12px sans-serif;
-                                                                   pointer-events: none;
-                                                                   color: white;
-                                                                   z-index: 10;
-                                                                   background: rgba(0,0,0,0.65);
-                                                                   border-radius: 3px;
-                                                                   padding: 5px;
-                                                                   opacity: 0;
-                                                                   text-align: center;
-                                                                   margin-bottom: 5px;" />
-    return (
-      <div className={styles.skilltreeBackground}>
-        <Sunburst
-          data={data}
-          scale="linear" // or exponential
-          keyId="skilltree"
-          width={this.state.width}
-          height={this.state.height}
-          tooltipContent={tooltipContent}
-        >
-        </Sunburst>
-      </div>
-    )
-  }
+  const tooltipContent = <div className="sunburstTooltip" style="display: inline;
+                                                                 position: absolute;
+                                                                 max-width: 320px;
+                                                                 white-space: nowrap;
+                                                                 font: 12px sans-serif;
+                                                                 pointer-events: none;
+                                                                 color: white;
+                                                                 z-index: 10;
+                                                                 background: rgba(0,0,0,0.65);
+                                                                 border-radius: 3px;
+                                                                 padding: 5px;
+                                                                 opacity: 0;
+                                                                 text-align: center;
+                                                                 margin-bottom: 5px;" />
+  return (
+    <div className={styles.skilltreeBackground}>
+      <Sunburst
+        data={data}
+        scale="linear" // or exponential
+        keyId="skilltree"
+        width={width}
+        height={height}
+        tooltipContent={tooltipContent}
+      >
+      </Sunburst>
+    </div>
+  )
 }
 
 export default SkillTree
--- components/sunburst.js
+++ components/sunburst.js
@@ -1,84 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
 import * as d3 from 'd3v4'
 import * as utils from '../utils/sunburstUtils'
 import styles from '../styles/skilltree.module.scss'
-/**
- * Sunburst Chart React Stateless Component with the following allowable Props *
- * data => JSON Array - Typically same for every Sunburst Chart *
- * scale => String - Options: linear | exponential - Linear renders each arc with same radii, Exponential reduces gradually by SquareRoot *
- * onSelect => Function - Called on Arc Click for re-rendering the chart and passing back to User as props *
- * tooltip => Boolean - Display Tooltip or not *
- * tooltipContent => HTMLNode - Customized Node for Tooltip rendering *
- * keyId => String - Unique Id for Chart SVG *
- * width => Integer - Width of the Chart Container *
- * height => Integer - Height of the Chart Container *
- */
-class Sunburst extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { mouse_x: 0, mouse_y: 0, disabled: false };
-  }
 
-  _onMouseMove(e) {
-    this.setState({ mouse_x: e.screenX, mouse_y: e.screenY + window.pageYOffset });
-  }
+const Sunburst = ({ data, width, height, onSelect, tooltipContent, keyId }) => {
+  const [disabled, setDisabled] = useState(false)
 
-  createSunburst() {
-    this.renderSunburst(this.props);
-    this.setState({ disabled: true })
-  }
-
-  update(root, firstBuild, svg, partition, hueDXScale, x, y, radius, arc, middleArc, node, self) {
+  const update = (root, firstBuild, svg, partition, hueDXScale, x, y, radius, arc, middleArc, node) => {
     if (firstBuild) {
-      firstBuild = false;
+      firstBuild = false
       // Handle clicking on the different levels
       function click(d) {
-        node = d;
-        self.props.onSelect && self.props.onSelect(d);
+        node = d
+        onSelect && onSelect(d)
         svg.selectAll('path.main-arc')
           .transition().duration(1000)
-          .attrTween('d', utils.arcTweenZoom(d, x, y, radius, arc));
+          .attrTween('d', utils.arcTweenZoom(d, x, y, radius, arc))
         svg.selectAll('path.hidden-arc')
           .transition().duration(1000)
-          .attrTween('d', utils.arcTweenZoom(d, x, y, radius, middleArc));
+          .attrTween('d', utils.arcTweenZoom(d, x, y, radius, middleArc))
         svg.selectAll('.path-label')
           .transition().duration(1000)
           .styleTween('display', d => () => utils.textFits(d, x, y) ? null : 'none')
       }
       // Create the tooltip div
-      const tooltipContent = this.props.tooltipContent;
-      //const tooltipContent = <div className={styles.sunburstTooltip}/>;
-      const tooltip = d3.select(`#${self.props.keyId}`)
-        .append(tooltipContent.type)
-      Object.keys(tooltipContent.props).forEach((key) => {
-        tooltip.attr(key, tooltipContent.props[key]);
+      const tooltipCont = tooltipContent
+      const tooltip = d3.select(`#${keyId}`)
+        .append(tooltipCont.type)
+      Object.keys(tooltipCont.props).forEach((key) => {
+        tooltip.attr(key, tooltipCont.props[key])
       });
       const slice = svg.selectAll('slice').data(partition(root).descendants()).enter().append('g')
         .attr('class', 'slice')
-        .style('opacity', 1);
+        .style('opacity', 1)
       slice.append('path')
         .attr('class', 'main-arc')
         // Set the fill colour
         .style('fill', (d) => {
-          let hue;
-          const current = d;
+          let hue
+          const current = d
           // Set the color of the root node
           if (current.depth === 0) {
-            //return '#33cccc';
             return '#eee';
           }
           // Set the color of the first level nodes
           if (current.depth <= 1) {
-            hue = hueDXScale(d.x0);
-            current.fill = d3.hsl(hue, 0.5, 0.6);
-            return current.fill;
+            hue = hueDXScale(d.x0)
+            current.fill = d3.hsl(hue, 0.5, 0.6)
+            return current.fill
           }
           // Set the child nodes in the subsequent levels as different shades
-          current.fill = current.parent.fill.brighter(0.35);
-          const hsl = d3.hsl(current.fill);
-          hue = hueDXScale(current.x0);
-          const colorshift = hsl.h + (hue / 4);
-          return d3.hsl(colorshift, hsl.s, hsl.l);
+          current.fill = current.parent.fill.brighter(0.35)
+          const hsl = d3.hsl(current.fill)
+          hue = hueDXScale(current.x0)
+          const colorshift = hsl.h + (hue / 4)
+          return d3.hsl(colorshift, hsl.s, hsl.l)
         })
         // Style the lines between segments
         .attr('stroke', '#eee')
@@ -87,34 +63,34 @@ class Sunburst extends React.Component {
         .on('click', d => click(d, node, svg, self, x, y, radius, arc))
         // Create the tooltip and set the content
         .on('mouseover', function (d) {
-          d3.select(this).style('cursor', 'pointer');
-          tooltip.html(() => { const name = utils.formatNameTooltip(d); return name; });
+          d3.select(this).style('cursor', 'pointer')
+          tooltip.html(() => { const name = utils.formatNameTooltip(d); return name; })
           // Translate to cartesian coordinates, midway through the arc
-          let r = y(d.y1);
-          let theta = x((d.x0 + d.x1) / 2);
-          let ypos = r * Math.cos(theta);
-          let xpos = r * Math.sin(theta);
+          let r = y(d.y1)
+          let theta = x((d.x0 + d.x1) / 2)
+          let ypos = r * Math.cos(theta)
+          let xpos = r * Math.sin(theta)
           // Shift the anchor point depending on the quadrant
-          let w = tooltip.node().getBoundingClientRect().width;
-          let h = tooltip.node().getBoundingClientRect().height;
-          if (xpos < 0) { xpos = xpos - w; }
-          if (ypos < 0) { ypos = ypos - h; }
+          let w = tooltip.node().getBoundingClientRect().width
+          let h = tooltip.node().getBoundingClientRect().height
+          if (xpos < 0) { xpos = xpos - w }
+          if (ypos < 0) { ypos = ypos - h }
           tooltip
             .style('bottom', `${ypos}px`)
-            .style('left', `${xpos}px`);
-          return tooltip.transition().duration(50).style('opacity', 1);
+            .style('left', `${xpos}px`)
+          return tooltip.transition().duration(50).style('opacity', 1)
         })
         // Set the tooltip back to normal
         .on('mouseout', function () {
-          d3.select(this).style('cursor', 'default');
-          tooltip.transition().duration(50).style('opacity', 0);
+          d3.select(this).style('cursor', 'default')
+          tooltip.transition().duration(50).style('opacity', 0)
           return null;
         });
       slice.append('path')
         .attr('class', 'hidden-arc')
         // Set ID for the text to get the reference of
         .attr('id', d => `hidden-arc-${d.data.name}`)
-        .style('fill', 'none');
+        .style('fill', 'none')
       // Create the arcs for each node
       const label = slice.append('text')
         .attr('class', 'path-label')
@@ -128,88 +104,89 @@ class Sunburst extends React.Component {
         .attr('stroke-width', 5)
         .attr('stroke-linejoin', 'round')
         .attr('href', d => `#hidden-arc-${d.data.name}`)
-        .text(d => `${d.data.name}`);
+        .text(d => `${d.data.name}`)
       label.append('textPath')
         .attr('class', 'text-stroke')
         .attr('startOffset', '25%')
         .attr('href', d => `#hidden-arc-${d.data.name}`)
-        .text(d => `${d.data.name}`);
+        .text(d => `${d.data.name}`)
     } else {
       // If the sunburst has already been created just select all visible paths
-      svg.selectAll('path').data(partition(root).descendants());
+      svg.selectAll('path').data(partition(root).descendants())
     }
     // Control initial opening animation
     svg.selectAll('path.main-arc')
       .transition().duration(1000)
-      .attrTween('d', (d, i) => utils.arcTweenData(d, i, node, x, arc));
+      .attrTween('d', (d, i) => utils.arcTweenData(d, i, node, x, arc))
     svg.selectAll('path.hidden-arc')
       .transition().duration(1000)
-      .attrTween('d', (d, i) => utils.arcTweenData(d, i, node, x, middleArc));
+      .attrTween('d', (d, i) => utils.arcTweenData(d, i, node, x, middleArc))
     svg.selectAll('.path-label')
       .transition().duration(1000)
       .styleTween('display', d => () => utils.textFits(d, x, y) ? null : 'none')
-    svg.selectAll('text.path-label').select('textPath.text-contour');
-    svg.selectAll('text.path-label').select('textPath.text-stroke');
+    svg.selectAll('text.path-label').select('textPath.text-contour')
+    svg.selectAll('text.path-label').select('textPath.text-stroke')
   }
 
-  renderSunburst(props) {
-    if (props.data) {
-      const self = this,
-        // Set total size of chart
-        gWidth = props.width,
-        gHeight = props.height,
-        radius = (Math.min(gWidth, gHeight) / 2) - 10,
-        // Create the svg element and put it in the right place
-        svg = d3.select('svg').append('g').attr('transform', `translate(${gWidth / 2},${gHeight / 2})`),
-        // Controls how far around the sunburst goes
-        x = d3.scaleLinear().range([0, 2 * Math.PI]),
-        // Controls the relative size of each layer
-        y = d3.scaleLinear().range([0, radius]),
-        partition = d3.partition(),
-        // Controls the arc geometry for each node
-        arc = d3.arc()
-          .startAngle(d => Math.max(0, Math.min(2 * Math.PI, x(d.x0))))
-          .endAngle(d => Math.max(0, Math.min(2 * Math.PI, x(d.x1))))
-          .innerRadius(d => Math.max(0, y(d.y0)))
-          .outerRadius(d => Math.max(0, y(d.y1))),
-        // Line through the middle of each arc to attach text to, flip angle half way to get text orientation right
-        middleArc = d3.arc()
-          .startAngle(d => ((x(d.x0) + x(d.x1)) / 2 > Math.PI / 2 && (x(d.x0) + x(d.x1)) / 2 < Math.PI * 3 / 2)
-            ? Math.max(0, Math.min(2 * Math.PI, x(d.x1)))
-            : Math.max(0, Math.min(2 * Math.PI, x(d.x0))))
-          .endAngle(d => ((x(d.x0) + x(d.x1)) / 2 > Math.PI / 2 && (x(d.x0) + x(d.x1)) / 2 < Math.PI * 3 / 2)
-            ? Math.max(0, Math.min(2 * Math.PI, x(d.x0)))
-            : Math.max(0, Math.min(2 * Math.PI, x(d.x1))))
-          .innerRadius(d => Math.max(0, (y(d.y0) + y(d.y1)) / 2))
-          .outerRadius(d => Math.max(0, (y(d.y0) + y(d.y1)) / 2)),
-        // Controls to color scaling of the first level nodes
-        hueDXScale = d3.scaleLinear()
-          .domain([0, 1])
-          .range([0, 360]),
-        // Create a hierarchical layout from the data
-        rootData = d3.hierarchy(props.data);
-      const firstBuild = true;
-      const node = rootData;
+  const renderSunburst = () => {
+    if (data) {
+      const self = this
+      // Set total size of chart
+      const radius = (Math.min(width, height) / 2) - 10
+      // Create the svg element and put it in the right place
+      const svg = d3.select('svg').append('g').attr('transform', `translate(${width / 2},${height / 2})`)
+      // Controls how far around the sunburst goes
+      const x = d3.scaleLinear().range([0, 2 * Math.PI])
+      // Controls the relative size of each layer
+      const y = d3.scaleLinear().range([0, radius])
+      const partition = d3.partition()
+      // Controls the arc geometry for each node
+      const arc = d3.arc()
+        .startAngle(d => Math.max(0, Math.min(2 * Math.PI, x(d.x0))))
+        .endAngle(d => Math.max(0, Math.min(2 * Math.PI, x(d.x1))))
+        .innerRadius(d => Math.max(0, y(d.y0)))
+        .outerRadius(d => Math.max(0, y(d.y1)))
+      // Line through the middle of each arc to attach text to, flip angle half way to get text orientation right
+      const middleArc = d3.arc()
+        .startAngle(d => ((x(d.x0) + x(d.x1)) / 2 > Math.PI / 2 && (x(d.x0) + x(d.x1)) / 2 < Math.PI * 3 / 2)
+          ? Math.max(0, Math.min(2 * Math.PI, x(d.x1)))
+          : Math.max(0, Math.min(2 * Math.PI, x(d.x0))))
+        .endAngle(d => ((x(d.x0) + x(d.x1)) / 2 > Math.PI / 2 && (x(d.x0) + x(d.x1)) / 2 < Math.PI * 3 / 2)
+          ? Math.max(0, Math.min(2 * Math.PI, x(d.x0)))
+          : Math.max(0, Math.min(2 * Math.PI, x(d.x1))))
+        .innerRadius(d => Math.max(0, (y(d.y0) + y(d.y1)) / 2))
+        .outerRadius(d => Math.max(0, (y(d.y0) + y(d.y1)) / 2))
+      // Controls to color scaling of the first level nodes
+      const hueDXScale = d3.scaleLinear()
+        .domain([0, 1])
+        .range([0, 360])
+      // Create a hierarchical layout from the data
+      const rootData = d3.hierarchy(data)
+      const firstBuild = true
+      const node = rootData
       // Add up the values of all children to set sizes of nodes
-      rootData.sum(d => d.size);
-      self.update(rootData, firstBuild, svg, partition, hueDXScale, x, y, radius, arc, middleArc, node, self); // GO!
+      rootData.sum(d => d.size)
+      update(rootData, firstBuild, svg, partition, hueDXScale, x, y, radius, arc, middleArc, node)
     }
   }
 
-  render() {
-    return (
-      <div id={this.props.keyId} className={styles.skilltree}>
-        <div className={styles.skilltreeMask}
-          style={{ pointerEvents: this.state.disabled ? 'none' : 'auto' }}
-          onClick={this.createSunburst.bind(this)}>Skills</div>
-        <div className={styles.sunburstViz}>
-          <svg style={{ width: parseInt(this.props.width, 10) || 480, height: parseInt(this.props.height, 10) || 400 }}
-            id={`${this.props.keyId}-svg`}
-            onMouseMove={this._onMouseMove.bind(this)} />
-        </div>
-      </div>
-    );
+  const createSunburst = () => {
+    renderSunburst()
+    setDisabled(true)
   }
+
+  return (
+    <div id={keyId} className={styles.skilltree}>
+      <div className={styles.skilltreeMask}
+        style={{ pointerEvents: disabled ? 'none' : 'auto' }}
+        onClick={createSunburst}>Skills</div>
+      <div className={styles.sunburstViz}>
+        <svg style={{ width: parseInt(width, 10) || 480, height: parseInt(height, 10) || 400 }}
+          id={`${keyId}-svg`}
+        />
+      </div>
+    </div>
+  )
 }
 
 export default Sunburst
