ff4651bd9c1f72d35c2d8b0074efc5a7a69503fa
Tom Brooks <tom.g.r.brooks@gmail.com>
2021-11-29 20:36:29 +0000

    Move AdventureGame to public

--- pages/cover.js
+++ pages/cover.js
@@ -3,6 +3,7 @@ import utilStyles from '../styles/utils.module.scss'
 import PostLayout from '../components/PostLayout.js'
 import CoverLetter from '../components/CoverLetter.js'
 import Alert from 'react-bootstrap/Alert'
+import DarkLink from '../components/UI/DarkLink'
 
 export default function CV() {
   return (
@@ -24,7 +25,7 @@ export default function CV() {
           Me too! So I attempted to use some natural language processing methods to do it for me and this is the result.
         </p>
         <p>
-          If you’re interested, you can read more about why it’s so hilariously bad here.
+          If you’re interested, you can read more about why it’s so hilariously bad <DarkLink link="/posts/deepcover" name="here"/>.
         </p>
         <Alert key={1} variant="warning">Warning: Runs MUCH faster in chrome and edge (because they allow you to use canvas drawing inside web workers which also allows tensorflow to use the WebGL backend rather than the CPU).</Alert>
         <CoverLetter />
